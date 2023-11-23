7196a2fc2e5547cbc6f6382e012064e927d887ea
MaceGibson <mace_gibson@yahoo.com>
2023-11-22 20:36:20 -0500

    Initial Commit

---
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -0,0 +1,18 @@
+package com.skilldistillery.foodtruck.app;
+
+import com.skilldistillery.foodtruck.entities.FoodTruck;
+
+public class FoodTruckApp {
+	
+	private FoodTruck [] fleet = new FoodTruck[5];
+	
+	public static void main(String[] args) {
+	//has a main
+		FoodTruckApp app = new FoodTruckApp();
+		app.run();
+	}
+	
+	public void run() {
+		//where rest of the code goes
+	}
+}
