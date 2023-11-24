c4410ea1ddd59d02c7a3f93aa7d344afde5de393
MaceGibson <mace_gibson@yahoo.com>
2023-11-24 15:27:42 -0500

    FoodTruck object version 1

--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -1,5 +1,7 @@
 package com.skilldistillery.foodtruck.app;
 
+import java.util.Scanner;
+
 import com.skilldistillery.foodtruck.entities.FoodTruck;
 
 public class FoodTruckApp {
@@ -8,11 +10,24 @@ public class FoodTruckApp {
 	
 	public static void main(String[] args) {
 	//has a main
+		//create an instance of the non-static.
 		FoodTruckApp app = new FoodTruckApp();
 		app.run();
 	}
 	
 	public void run() {
 		//where rest of the code goes
+		Scanner kb = new Scanner(System.in);
+		//get the food truck info via user prompt and input
+		System.out.println("Food Truck Name: ");
+		String name = kb.nextLine();
+		
+		System.out.println("Food Truck Food: ");
+		String food = kb.nextLine();
+		
+		System.out.println("Food Truck Numerical Rating (1-5, with 5 being the best): ");
+		int rating = kb.nextInt();
+		
+		FoodTruck [] name = name.setName();
 	}
 }
--- src/com/skilldistillery/foodtruck/entities/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -2,4 +2,48 @@ package com.skilldistillery.foodtruck.entities;
 
 public class FoodTruck {
 //no main
+
+	// private fields for the FoodTruck object
+	private static int nextTruckId = 1; // stretch goal
+	private int truckId;// stretch goal
+	private String name;
+	private String foodType;
+	private int rating;
+
+	// Constructor -easier than setters for this design
+
+	public FoodTruck(String name, String foodType, int truckRating) {
+		this.nextTruckId = nextTruckId++;// stretch goal incrementing
+		this.name = name;
+		this.foodType = foodType;
+		this.rating = truckRating;
+	}
+
+	// the following lines will be the eclipse gen setters and getters
+
+	public int getTruckId() {
+		return truckId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getFoodType() {
+		return foodType;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+	
+	// now a toString for display purposes
+	@Override  //apparently good practice and done by eclipse
+		public String toString() {
+		return "Truck ID: " + truckId +
+                "\nName: " + name +
+                "\nFood Type: " + foodType +
+                "\nRating: " + rating + "\n";
+		}
+
 }
